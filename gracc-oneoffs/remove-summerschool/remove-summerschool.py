775e922138dc0bb437202dfcf1d84d1f2f551e36
Derek Weitzel <djw8605@gmail.com>
2017-08-30 14:54:17 -0500

    Adding remove-summerschool.py

---
+++ gracc-oneoffs/remove-summerschool/remove-summerschool.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import elasticsearch
+from elasticsearch_dsl import Search, A, Q
+#import logging
+import operator
+import sys
+import os
+
+
+#logging.basicConfig(level=logging.WARN)
+es = elasticsearch.Elasticsearch(
+        ['https://gracc.opensciencegrid.org/q'],
+        timeout=300, use_ssl=True, verify_certs=False)
+
+osg_raw_index = 'gracc.osg.raw-*'
+
+
+s = Search(using=es, index=osg_raw_index)
+
+s = s.query("match", ProjectName="UserSchool2017")
+response = s.execute()
+
+print "Query took %i milliseconds" % response.took 
+
+print "Query got %i hits" % response.hits.total
+
+#update_id = "8c5816978fee6fc17718bcf81350d1f4"
+#print "About to update record with id: %s" % update_id
+#es.update(index="gracc.osg.raw3-2017.07", doc_type='JobUsageRecord', id=update_id, body={'doc': {'VOName': 'UserSchool2017'}}) 
+
+for hit in s.scan():
+    
+    if hit.ProjectName != hit.VOName:
+        print "Updating %s with VOName = %s to VOName = %s" % (hit.meta.id, hit.VOName, hit.ProjectName)
+        es.update(index=hit.meta.index, doc_type=hit.meta.doc_type, id=hit.meta.id, body={'doc': {'VOName': hit.ProjectName}})
+        
+    

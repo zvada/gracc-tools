b92b2f84960882024887ed154bbb25eac9f81c3a
Derek Weitzel <djw8605@gmail.com>
2017-08-31 10:46:25 -0500

    Adding summary deletes

--- gracc-oneoffs/remove-summerschool/remove-summerschool.py
+++ gracc-oneoffs/remove-summerschool/remove-summerschool.py
@@ -14,7 +14,7 @@ es = elasticsearch.Elasticsearch(
         timeout=300, use_ssl=True, verify_certs=False)
 
 osg_raw_index = 'gracc.osg.raw-*'
-
+osg_summary_index = 'gracc.osg.summary'
 
 s = Search(using=es, index=osg_raw_index)
 
@@ -35,4 +35,18 @@ for hit in s.scan():
         print "Updating %s with VOName = %s to VOName = %s" % (hit.meta.id, hit.VOName, hit.ProjectName)
         es.update(index=hit.meta.index, doc_type=hit.meta.doc_type, id=hit.meta.id, body={'doc': {'VOName': hit.ProjectName}})
         
-    
+# Also, in the summary, we want to delete records that match:
+# - ProjectName: UserSchool2017
+# - VOName != ProjectName
+s = Search(using=es, index=osg_summary_index)
+s = s.query("match", ProjectName="UserSchool2017")
+response = s.execute()
+print "Query took %i milliseconds" % response.took 
+
+print "Query got %i hits" % response.hits.total
+
+for hit in s.scan():
+    if hit.ProjectName != hit.VOName:
+        print "Deleting id = %s, with ProjectName = %s and VOName = %s" % (hist.meta.id, hit.ProjectName, hit.VOName)
+        es.delete(index=hit.meta.index, doc_type=hit.meta.doc_type, id=hit.meta.id)
+
